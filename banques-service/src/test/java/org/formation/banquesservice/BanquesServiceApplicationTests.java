8a4bc739a7807c1dca22a72bc73435296077221e
David THIBAU <david.thibau@gmail.com>
2024-02-08 17:44:25 +0100

    Fix contextLoad Test + dependence native

--- banques-service/src/test/java/org/formation/banquesservice/BanquesServiceApplicationTests.java
+++ banques-service/src/test/java/org/formation/banquesservice/BanquesServiceApplicationTests.java
@@ -2,9 +2,18 @@ package org.formation.banquesservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BanquesServiceApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.1-alpine");
 
 	@Test
 	void contextLoads() {
